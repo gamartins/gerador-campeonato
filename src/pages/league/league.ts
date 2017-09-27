861ba13c7b3d146700acbf4950ad52b97c4a3b2d
Gabriel Angelo <gabriel.angelo91@gmail.com>
2017-09-27 13:13:52 -0300

    Showing leagues on LeaguePage

--- src/pages/league/league.ts
+++ src/pages/league/league.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 import { NewLeaguePage } from '../new-league/new-league';
+import { LeagueProvider } from '../../providers/league/league';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-league',
   templateUrl: 'league.html',
 })
 export class LeaguePage {
+  leagueList = []
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,) {
+  constructor(
+    public navCtrl: NavController,
+    public leagueProvider: LeagueProvider,) {
+
+  }
+
+  ionViewWillEnter() {
+    this.leagueList = []
+    this.leagueProvider.leagueList.subscribe(data => 
+      data.forEach(element => this.leagueList.push(element))
+    )
+
+    console.log(this.leagueList)
   }
 
   openNewLeaguePage() {
